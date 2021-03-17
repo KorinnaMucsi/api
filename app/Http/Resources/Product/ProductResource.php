724f342e347f4acfc7d547f38cd3ec0ad5b090cb
KorinnaMucsi <korinna.mucsi@gmail.com>
2021-03-17 15:17:16 +0100

    Changing product data within the resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -20,7 +20,16 @@ class ProductResource extends JsonResource
             'description' => $this->detail,
             'price' => $this->price,
             'discount' => $this->discount,
-            'stock' => $this->stock
+            //if stock is null, or 0, the value should be Out of stock
+            'stock' => ($this->stock == 0 || is_null($this->stock)) ? 'Out of stock' : $this->stock,
+            //we calculate the average rating if there is at least on rating given for the product
+            //(rating: sum of the stars given/total number of stars)
+            'rating' => ($this->reviews->count() > 0) ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            //we calculate the totalPrice based on the discount
+            'totalPrice' => ($this->discount != 0 && !is_null($this->discount)) ? round((1-$this->discount/100) * $this->price,2) : $this->price,
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
